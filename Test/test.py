da2e3fafb78a2ee3e4efeec532331e9499bd4ec9
Söll, Jeremias <e12125488@student.tuwien.ac.at>
2024-04-06 14:28:26 +0200

    deleted old stuff

--- Test/test.py
+++
@@ -1,9 +0,0 @@
-import os, sys
-
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.dirname(SCRIPT_DIR))
-
-from library.TestClass import A
-
-a0 = A(x = 3.14)
-print("value: ", a0.x, "report-Method: ", a0.report())
